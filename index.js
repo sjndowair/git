bc814aa09358e962eeddaf6953856a0fcd31515f
sjndowair <wodnjsdl1125@naver.com>
2025-04-21 18:37:09 +0900

    랜덤 숫자 반환하기

--- index.js
+++ index.js
@@ -2,3 +2,19 @@ const a = 10;
 const b = 20;
 
 
+
+const randomNumber = () => {
+    return Math.floor(Math.random() * 100 + 1);
+}
+
+
+
+let temp = []
+const myNumber = () => {
+    for(let i = 0; i < 6; i++){
+        temp.push(randomNumber());
+    }
+    return temp
+}
+
+console.log(myNumber())
